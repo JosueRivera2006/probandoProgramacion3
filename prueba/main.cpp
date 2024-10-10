414acb3872515b4ee3b736f5dd9d9273f8f62452
JosueRivera <josue.gabriel.rivera1@gmail.com>
2024-10-10 13:42:47 -0600

    prueba1

--- prueba/main.cpp
+++ prueba/main.cpp
@@ -1,14 +1,6 @@
-//
-//  main.cpp
-//  prueba
-//
-//  Created by Alejandro Rivera on 10/10/24.
-//
-
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
 }
