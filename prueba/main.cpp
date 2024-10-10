cb0c26f94fdf83c10fc4350b662798e16eacfdea
JosueRivera <josue.gabriel.rivera1@gmail.com>
2024-10-10 13:50:10 -0600

    prueba2

--- prueba/main.cpp
+++ prueba/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    
+int main() {
+    bool bandera = true;
+    if(bandera) {
+        cout << "La bandera es verdadera" << endl;
+    } else {
+        cout << "la bandera es falsa" << endl;
+    }
 }
