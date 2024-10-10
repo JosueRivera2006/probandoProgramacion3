24b7b3b0ef06ab21693f2bf2089be75702d1c010
JosueRivera <josue.gabriel.rivera1@gmail.com>
2024-10-10 11:31:04 -0600

    Initial Commit

---
+++ prueba/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  prueba
+//
+//  Created by Alejandro Rivera on 10/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
